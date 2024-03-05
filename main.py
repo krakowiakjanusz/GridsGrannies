c3bd6b92c32bb9ecdcc21fe27675945108293535
krakowiakjanusz <146106696+krakowiakjanusz@users.noreply.github.com>
2024-03-05 20:14:02 +0000

    Initial outline.

---
+++ main.py
@@ -0,0 +1,47 @@
+# Welcome text.
+
+
+# Player chooses grannies or children to play as.
+
+
+# Display battleship-style grid.
+
+
+# Ask for an input of which square to place an item on.
+
+
+# Do that 5 times.
+
+
+# Randomly populate the squares of the opposing team.
+
+
+# Display 5 x-axis positions to shoot from.
+
+
+# Choose position.
+
+
+# Display 5 y-axis positions to hit.
+
+
+# Choose position.
+
+
+# Display hit or miss message.
+
+
+# Opposing team does their turn.
+
+
+# Display hit or miss message.
+
+
+# Loop until win or lose condition is met.
+
+
+# 'You win' screen. 
+
+
+# Restart prompt.
+
